f3b2e1ab80fc4b934ab59a8e52663b34b03f9482
jonathan <jona.stampella@gmail.com>
2022-08-08 09:17:14 -0300

    eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+		node: true,
+		jest: true, // Add this line!
+	},
+	settings: {
+		'import/resolver': {
+			typescript: {},
+		},
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:@typescript-eslint/recommended',
+		'prettier', // Add this line!
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier'],
+	rules: {
+		'react/react-in-jsx-scope': 'off',
+		camelcase: 'error',
+		'spaced-comment': 'error',
+		quotes: ['error', 'single'],
+		'no-duplicate-imports': 'error',
+	},
+};
