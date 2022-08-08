f5ba247d959623359dc2effc06e68fe33fc12169
jonathan <jona.stampella@gmail.com>
2022-08-08 12:28:16 -0300

    add button change Theme

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,22 @@
-import {
-	GithubLink,
-	HeaderContainer,
-	Title,
-	HeaderIconsContainer,
-} from './styled';
+import { HeaderContainer, Title, HeaderIconsContainer } from './styled';
+import DarkModeToggle from 'react-dark-mode-toggle';
+import { AppStore } from '../../store/store';
+import { useDispatch, useSelector } from 'react-redux';
+import { toggleDarkMode } from '../../store/reducers/appReducer';
 
 export const Header = () => {
+	const dispatch = useDispatch();
+	const isDarkMode = useSelector((state: AppStore) => state.app.darkMode);
 	return (
 		<HeaderContainer>
 			<Title>Weather - PPAY</Title>
-			<HeaderIconsContainer></HeaderIconsContainer>
+			<HeaderIconsContainer>
+				<DarkModeToggle
+					checked={isDarkMode}
+					onChange={() => dispatch(toggleDarkMode())}
+					size={90}
+				/>
+			</HeaderIconsContainer>
 		</HeaderContainer>
 	);
 };
